ba910f0a947e5f965357501c474f5f9346d831a2
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2022-10-18 23:14:19 +0200

    Importer le template et creer la migration et le model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,44 +1,145 @@
 <?php
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Notifications\Notifiable;
+    use Laravel\Sanctum\HasApiTokens;
 
-namespace App\Models;
+    class User extends Authenticatable{
+        use HasApiTokens, HasFactory, Notifiable;
+        protected $table = 'users';
+        protected $primaryKey = 'id_user';
+        public $timestamps = false;
+        public $incrementing = false;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array<int, string>
+         */
+        protected $fillable = [
+            'id_user',
+            'email',
+            'password',
+            'cin',
+            'nom',
+            'prenom',
+            'genre',
+            'naissance',
+            'mobile',
+            'adresse',
+            'image',
+            'type',
+            'date_creation'
+        ];
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
+        /**
+         * The attributes that should be hidden for serialization.
+         *
+         * @var array<int, string>
+         */
+        protected $hidden = [
+            'password',
+        ];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+        public function getIdUserAttribute(){
+            return $this->attributes['id_user'];
+        }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+        public function getEmailUserAttribute(){
+            return $this->attributes['email'];
+        }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
+        public function setEmailUserAttribute($value){
+            $this->attributes['email'] = $value;
+        }
+
+        public function getPasswordUserAttribute(){
+            return $this->attributes['password'];
+        }
+
+        public function setPasswordUserAttribute($value){
+            $this->attributes['password'] = $value;
+        }
+
+        public function getCinUserAttribute(){
+            return $this->attributes['cin'];
+        }
+
+        public function setCinUserAttribute($value){
+            $this->attributes['cin'] = $value;
+        }
+
+        public function getNomUserAttribute(){
+            return $this->attributes['nom'];
+        }
+
+        public function setNomUserAttribute($value){
+            $this->attributes['nom'] = $value;
+        }
+
+        public function getPrenomUserAttribute(){
+            return $this->attributes['prenom'];
+        }
+
+        public function setPrenomUserAttribute($value){
+            $this->attributes['prenom'] = $value;
+        }
+
+        public function getGenreUserAttribute(){
+            return $this->attributes['genre'];
+        }
+
+        public function setGenreUserAttribute($value){
+            $this->attributes['genre'] = $value;
+        }
+
+        public function getNaissanceUserAttribute(){
+            return $this->attributes['naissance'];
+        }
+
+        public function setNaissanceUserAttribute($value){
+            $this->attributes['naissance'] = $value;
+        }
+
+        public function getMobileUserAttribute(){
+            return $this->attributes['mobile'];
+        }
+
+        public function setMobileUserAttribute($value){
+            $this->attributes['mobile'] = $value;
+        }
+
+        public function getAdresseUserAttribute(){
+            return $this->attributes['adresse'];
+        }
+
+        public function setAdresseUserAttribute($value){
+            $this->attributes['adresse'] = $value;
+        }
+
+        public function getImageUserAttribute(){
+            return $this->attributes['image'];
+        }
+
+        public function setImageUserAttribute($value){
+            $this->attributes['image'] = $value;
+        }
+
+        public function getTypeUserAttribute(){
+            return $this->attributes['type'];
+        }
+
+        public function setTypeUserAttribute($value){
+            $this->attributes['type'] = $value;
+        }
+
+        public function getDateCreationUserAttribute(){
+            return $this->attributes['date_creation'];
+        }
+
+        public function setDateCreationUserAttribute($value){
+            $this->attributes['date_creation'] = $value;
+        }
+    }
+?>
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,36 +1,41 @@
 <?php
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    return new class extends Migration{
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up(){
+            Schema::create('users', function (Blueprint $table) {
+                $table->collation = 'utf8_general_ci';
+                $table->charset = 'utf8';
+                $table->id('id_user');
+                $table->string('email', 999);
+                $table->string('password', 999);
+                $table->integer('cin', false, false)->default(0);
+                $table->string('nom',500)->defaul('Aucun');
+                $table->string('prenom',500)->defaul('Aucun');
+                $table->string('genre',150)->defaul('Non spécifié');
+                $table->date('naissance')->default(DB::raw('CURRENT_TIMESTAMP'))->setTimezone('GMT');
+                $table->integer('mobile', false, false)->default(0);
+                $table->string('adresse',500)->defaul('Aucun');
+                $table->string('image',800)->defaul('Aucun');
+                $table->string('type',150)->defaul('Utilisateur');
+                $table->date('date_creation')->default(DB::raw('CURRENT_TIMESTAMP'))->setTimezone('GMT');
+            });
+        }
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
-};
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down(){
+            Schema::dropIfExists('users');
+        }
+    };
+?>
