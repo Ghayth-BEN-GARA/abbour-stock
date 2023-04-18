8727b98ef6763cc117818521a74a88292a3a8542
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2023-04-19 01:33:55 +0200

    Ajouter les statistiques pour chaque mois

--- app/Http/Livewire/ListeStatistiques.php
+++ app/Http/Livewire/ListeStatistiques.php
@@ -9,6 +9,9 @@
     use App\Models\Article;
     use App\Models\HistoriquePrixArticle;
     use App\Models\FactureVente;
+    use App\Models\ReglementVente;
+    use DB;
+    use Carbon\Carbon;
 
     class ListeStatistiques extends Component{
         public function render(){
@@ -54,6 +57,20 @@
 
         public function getNbrFacturesVentes(){
             return FactureVente::count();
+        }
+
+        public function getPrixDate(){
+            $result = DB::select(DB::raw("SELECT SUM(reg.somme_reglement_vente) AS somme_ventes, MONTH(reg.date_reglement_vente) AS date_ventes FROM reglements_ventes as reg WHERE YEAR(reg.date_reglement_vente) = YEAR(CURDATE())
+            GROUP BY MONTH(reg.date_reglement_vente)
+            ORDER BY reg.date_reglement_vente DESC"));
+           
+           $data = "";
+
+            foreach ($result as $value) {
+                $data .= "['".Carbon::parse($value->date_ventes)->addMonths(1)->format("F")."', ".number_format($value->somme_ventes, 3)."],";
+            }
+
+           return $data;
         }
     }
 ?>
--- resources/views/livewire/liste-statistiques.blade.php
+++ resources/views/livewire/liste-statistiques.blade.php
@@ -73,4 +73,52 @@
             </div>
         </div>
     </div>
+    <div class = "row g-4 mb-4">
+        <div class = "col-12 col-lg-12">
+            <div class = "app-card app-card-chart h-100 shadow-sm">
+                <div class = "app-card-header p-3">
+                    <div class = "row justify-content-between align-items-center">
+                        <div class = "col-auto">
+						    <h4 class = "app-card-title">Statistiques des ventes</h4>
+						</div>
+                        <div class = "col-auto">
+							<div class = "card-header-action">
+								<a href = "javascript:void(0)" style = "color:#F7941E">Par mois</a>
+							</div>
+						</div>
+                    </div>
+                </div>
+                <div class = "app-card-body p-3 p-lg-4">
+                    <div class = "chart-container">
+				        @if($nbr_ventes < 1)
+                            <p class = "text-center">Aucune vente n'a été créée ce mois-ci.</p>
+                        @else
+                            <div class = "mt-3" id = "curve_chart"></div>
+                        @endif
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src = "https://www.gstatic.com/charts/loader.js"></script>
+    <script type = "text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Date', 'Totale'],
+                <?php echo $this->getPrixDate() ?>
+            ]);
+            var options = {
+                pointSize: 10,
+                title: 'Année: '+new Date().getFullYear(),
+                curveType: 'function',
+                legend: { position: 'bottom' },
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+            chart.draw(data, options);
+        }
+    </script>
 </div>
